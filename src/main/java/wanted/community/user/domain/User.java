f42633a59987c88df54e926268b16d03a268d4e3
Chan <33cks1423@naver.com>
2023-08-05 17:07:02 +0900

    feat(user):  role 추가 - [#1]

--- src/main/java/wanted/community/user/domain/User.java
+++ src/main/java/wanted/community/user/domain/User.java
@@ -2,6 +2,7 @@ package wanted.community.user.domain;
 
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 import wanted.community.user.application.port.PasswordEncoderHolder;
 
@@ -12,23 +13,30 @@ public class User {
     private final Long id;
     private final Email email;
     private final Password password;
+    private final Role role;
 
     @Builder
-    public User(Long id, Email email, Password password) {
+    public User(Long id, Email email, Password password, Role role) {
         this.id = id;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
 
     public static User create(String email, String password, PasswordEncoderHolder passwordEncoderHolder) {
         return User.builder()
                 .email(Email.create(email))
                 .password(Password.create(password, passwordEncoderHolder))
+                .role(Role.USER)
                 .build();
     }
 
     public Long getId() {
         return id;
+    }
+
+    public Role getRole() {
+        return role;
     }
 
     public String getEmail() {
@@ -38,4 +46,5 @@ public class User {
     public String getPassword() {
         return password.getValue();
     }
+
 }
